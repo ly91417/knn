b3f8d91cd15224ec3f6bdfefa9c34adf8bd1c675
Ying Li <ly91417@gmail.com>
2016-10-27 23:45:21 -0500

    reclassified it into pacages

---
+++ src/kNN/example.java
@@ -0,0 +1,34 @@
+package kNN;
+import weka.core.Instance;
+public class example implements Comparable<example>{
+	private Instance instance;
+	private double distance;
+	public example(Instance in){
+		this.instance = in;
+	}
+	public example(Instance in, double distance) {
+		this.instance = in;
+		this.distance = distance;
+	}
+	public Instance getInstance() {
+		return this.instance;
+	}
+	public double getDistance() {
+		return this.distance;
+	}
+	public void setInstance(Instance in) {
+		this.instance = in;
+	}
+	public void setDistance(double distance) {
+		this.distance = distance;
+	}
+	public int compareTo(example o) {
+		// TODO Auto-generated method stub
+		if(this.distance > o.distance) return 1;
+		else if(this.distance == o.distance) { return 0;
+		}
+		else {
+			return -1;
+		}
+	}
+}
